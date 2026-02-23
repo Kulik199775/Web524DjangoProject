1ca49b7cb4435acc3313ee9edde1aa496acf93a0
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-02-23 16:03:50 +0300

    1) Добавлен класс StyleFormMixin в users/forms и обновлены все остальные классы 2) Добавлен класс StyleFormMixin в dogs/forms и обновлены все остальные классы

--- users/forms.py
+++ users/forms.py
@@ -2,13 +2,19 @@ from django import forms
 
 from users.models import User
 
-class UserForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+class UserForm(StyleFormMixin,forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone')
         # exclude = ('is_active',)
 
-class UserRegisterForm(forms.ModelForm):
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
@@ -23,11 +29,11 @@ class UserRegisterForm(forms.ModelForm):
             raise forms.ValidationError('Ошибка! Пароли не совпадают!')
         return cd['password2']
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
     email = forms.EmailField(label='email')
     password = forms.CharField(label='пароль', widget=forms.PasswordInput)
 
-class UserUpdateForm(forms.ModelForm):
+class UserUpdateForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'telegram', 'avatar')
