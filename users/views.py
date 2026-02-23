eaa0288de5f80a7c397f38788a606264536c2069
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-02-23 13:32:40 +0300

    1) Добавлен функционал авторизации пользователя

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserLoginForm
+
 
 def user_register_view(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            print(new_user)
-            print(form.cleaned_data['password'])
+            # print(new_user)
+            # print(form.cleaned_data['password'])
             new_user.set_password(form.cleaned_data['password']) #хэшируем пароль
             new_user.save()
             return HttpResponseRedirect(reverse('dogs:index'))
@@ -22,3 +24,21 @@ def user_register_view(request):
     }
     return render(request, 'users/user_register.html', context=context)
 
+
+def user_login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(email=cd['email'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect(reverse('dogs:index'))
+                else:
+                    return HttpResponse('Аккаунт не активен!')
+    context = {
+        'title': 'Авторизация',
+        'form': UserLoginForm
+    }
+    return render(request, 'users/user_login.html', context=context)
